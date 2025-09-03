599fd0e5f8fa69ea8c59bd53fb92a7fe90826f4f
nmellati <436477+duicker@users.noreply.github.com>
2025-09-03 06:50:55 +0200

    ajout tailwind et markdown. modification conform pour être dans la lignée de ce que demande lazyvim

--- lua/plugins/tailwind-colorized.lua
+++ lua/plugins/tailwind-colorized.lua
@@ -1,46 +1,15 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "NvChad/nvim-colorizer.lua",
   opts = {
-    filetypes = {
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-    },
+    filetypes = { "css", "scss", "html", "javascript", "typescript", "tsx", "jsx", "typescriptreact" },
     user_default_options = {
-      names = false, -- ignore les noms CSS classiques
-      css = false, -- on ne veut pas les couleurs classiques
-      css_fn = false, -- idem
-      tailwind = true, -- support Tailwind activé
-      mode = "foreground", -- défaut texte
+      names = true,
+      RGB = true,
+      RRGGBB = true,
+      RRGGBBAA = true,
+      rgb_fn = true,
+      hsl_fn = true,
+      tailwind = true, -- 👈 nécessaire pour activer Tailwind
     },
   },
-  config = function(_, opts)
-    local colorizer = require("colorizer")
-
-    -- première passe : classes text-* et autres
-    colorizer.setup(opts.filetypes, opts.user_default_options)
-
-    -- deuxième passe : classes bg-* → fond
-    colorizer.setup(opts.filetypes, {
-      mode = "background",
-      tailwind = true,
-      names = false,
-      css = false,
-      css_fn = false,
-      tailwind_regex = "bg%-[^%s]+", -- seules les classes bg-* sont colorées en fond
-    })
-
-    -- troisième passe : border-* et ring-* → texte
-    colorizer.setup(opts.filetypes, {
-      mode = "foreground",
-      tailwind = true,
-      names = false,
-      css = false,
-      css_fn = false,
-      tailwind_regex = "(border|ring)%-[^%s]+",
-    })
-  end,
 }
