8bf2b2532d2c9bae8c1e01600dfdde660aa2bfa7
nmellati <436477+duicker@users.noreply.github.com>
2025-09-07 06:55:00 +0200

    ajout du colorsheme. attention, penser à corriger le bug dans colorscheme du projet lazyvim (un get_theme au lieu de get...)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,78 @@
+return {
+  -- Ajouter Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          -- LazyVim specific integrations
+          alpha = true,
+          dashboard = true,
+          flash = true,
+          leap = true,
+          markdown = true,
+          mason = true,
+          neotest = true,
+          noice = true,
+          notify = true,
+          nvimtree = false,
+          semantic_tokens = true,
+          telescope = true,
+          treesitter_context = true,
+          which_key = true,
+        },
+      })
+    end,
+  },
+
+  -- Configurer LazyVim pour utiliser Catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
